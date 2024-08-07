cc945067061de98b8e576a059f7b51b29a1910e5
Jasark0 <jasonyen2004@gmail.com>
2024-08-07 17:02:43 -0500

    a

--- Scripts/to-do-list.js
+++ Scripts/to-do-list.js
@@ -1,7 +1,21 @@
 const tasks = [];
 
+updateTask();
+
+function updateTask(){
+  let todoTask = '';
+
+  for (let i=0; i<tasks.length; i++){
+  const todo = tasks[i];
+  const html = `<p>${todo}</p>`
+  todoTask += html;
+}
+
+document.querySelector('.js-todo-task').innerHTML = todoTask;
+}
 function addTodo(){
   tasks.push(document.querySelector('.js-name-input').value);
   console.log(tasks);
   document.querySelector('.js-name-input').value = '';
+  updateTask();
 }
