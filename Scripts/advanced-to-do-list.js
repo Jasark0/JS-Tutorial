1a2bac1466d06d116fa0a46b270c2f9828de504d
Jason Yen <jasonyen2004@gmail.com>
2024-08-13 10:43:06 -0500

    Advanced closure

--- Scripts/advanced-to-do-list.js
+++ Scripts/advanced-to-do-list.js
@@ -14,7 +14,7 @@ const tasks = [
   function updateTask(){
     let todoTask = '';
     
-    tasks.forEach(function(todoObject, index){
+    tasks.forEach((todoObject, index) => {
         const { name, dueDate } = todoObject;
         const html = `
         <div>
@@ -23,20 +23,35 @@ const tasks = [
         <div>
         ${dueDate}
         </div>
-        <button onclick="tasks.splice(${index}, 1); updateTask();" class="delete-button">Delete</button>
+        <button class="delete-button js-delete-todo-button">Delete</button>
         `;
         todoTask += html;
     });
   
     document.querySelector('.js-todo-task').innerHTML = todoTask;
-  
+    
+    document.querySelectorAll('.js-delete-todo-button')
+      .forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+          tasks.splice(index, 1); 
+          updateTask();
+        });
+      }); 
   }
+
+  document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+    addTodo();
+  });
 
   function addTodo(){
     const dateInputElement = document.querySelector('.js-due-date-input');
     const dueDate = dateInputElement.value;
     const name = document.querySelector('.js-name-input').value;
-  
+    
+    if (dueDate === '' && name === ''){
+      alert('Enter a Todo Name and a Due Date!');
+      return;
+    }
     tasks.push({name, dueDate});
     document.querySelector('.js-name-input').value = '';
     updateTask();
